b34d671e1f3e94e153ef1190a61f42f677c141e7
Aleksey Dmitriev <alexey@ioncloud.ru>
2016-04-26 11:31:55 +0300

    Initial Commit

---
+++ eyeCare/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  eyeCare
+//
+//  Created by Alexey Dmitriev on 26/04/16.
+//  Copyright © 2016 IonCloud. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
